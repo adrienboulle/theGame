6d0eb5321b7e1a20861b90e383c685f6cb8c3b8e
adboul <adrien.boulle@gadz.org>
2016-03-13 22:31:40 +0100

    ajout gulp

--- public/js/app/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('listeCourses', [
-	'ngResource',
-	'ui.router'
-	])
-angular.module('listeCourses', ['ngResource','ui.router'])
-	.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-		$locationProvider.html5Mode(true);
-		$urlRouterProvider.otherwise("/accueil");
-	});
