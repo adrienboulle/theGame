87bf8845fdb6febc48929dfc440ebf71ad452a66
adboul <adrien.boulle@gadz.org>
2016-03-17 23:23:36 +0100

    bootstram et form validation signup

--- sources/js/app/signup/signup.controller.js
+++ sources/js/app/signup/signup.controller.js
@@ -11,58 +11,21 @@
 
 	function SignupController($scope, $state, $resource, user, SignupService) {
 		
-		$scope.userInfo = {
-			username:'', 
-			password:'', 
-			passwordConfirmation:'',
-			erreurUsername: false,
-			erreurPassword: false,
-			erreurPasswordConfirmation: false,
-			isErreurUsername: function(){
-				return this.erreurUsername;
-			},
-			isErreurPassword: function(){
-				return this.erreurPassword;
-			},
-			isErreurPasswordConfirmation: function(){
-				return this.erreurPasswordConfirmation;
-			},
-			usernameValidation: function(){
-				if (this.username.length < 6 && this.username.length !=0){
-					this.erreurUsername = true;
-					return "6 caractères minimum";
-				}else{
-					this.erreurUsername = false;
-				}
-			},
-			passwordValidation: function(){
-				if (this.password.length < 8 && this.password.length !=0){
-					this.erreurPassword = true;
-					return "8 caractères minimum";
-				}else{
-					this.erreurPassword = false;
-				}
-			},
-			passwordConfirmationValidation: function(){
-				if (this.passwordConfirmation != this.password){
-					this.erreurPasswordConfirmation = true;
-					return "Non identique";
-				}else{
-					this.erreurPasswordConfirmation = false;
-				}
-			}
-		};
-
+		
 		$scope.erreur = '';
 
 		$scope.signup = function() {
-			SignupService.signup($scope.userInfo).then(function(data) {
-				$state.go('site.login', {}, {
-					reload: true
-				});
-			}, function(data) {
-				$scope.erreur = 'Erreur !';
-			})
+			if ($scope.userForm.$valid) {
+				SignupService.signup($scope.userInfo).then(function(data) {
+					$state.go('site.login', {}, {
+						reload: true
+					});
+				}, function(data) {
+					$scope.erreur = 'Erreur !';
+				})
+			} else {
+				$scope.errur = 'non valide';
+			}
 		}
 	}
 
