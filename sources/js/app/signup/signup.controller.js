a489dc1aa16e23512da12ab8ab74ddd00faf502c
romain <tg@tg.com>
2016-03-17 21:09:16 +0100

    ajout verif

--- sources/js/app/signup/signup.controller.js
+++ sources/js/app/signup/signup.controller.js
@@ -15,9 +15,40 @@
 			username:'', 
 			password:'', 
 			passwordConfirmation:'',
+			erreurUsername: false,
+			erreurPassword: false,
+			erreurPasswordConfirmation: false,
+			isErreurUsername: function(){
+				return this.erreurUsername;
+			},
+			isErreurPassword: function(){
+				return this.erreurPassword;
+			},
+			isErreurPasswordConfirmation: function(){
+				return this.erreurPasswordConfirmation;
+			},
 			usernameValidation: function(){
-				if (this.username.length < 5){
-					return "Yo pouleeeet"
+				if (this.username.length < 6){
+					this.erreurUsername = true;
+					return "6 caractères minimum";
+				}else{
+					this.erreurUsername = false;
+				}
+			},
+			passwordValidation: function(){
+				if (this.password.length < 8){
+					this.erreurPassword = true;
+					return "8 caractères minimum";
+				}else{
+					this.erreurPassword = false;
+				}
+			},
+			passwordConfirmationValidation: function(){
+				if (this.passwordConfirmation != this.password){
+					this.erreurPasswordConfirmation = true;
+					return "Non identique";
+				}else{
+					this.erreurPasswordConfirmation = false;
 				}
 			}
 		};
