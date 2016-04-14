7c7ce2138d5c4999f860e2e2bc260bc8fe8a560b
adboul <adrien.boulle@gadz.org>
2016-04-14 09:08:11 +0200

    ajout scroll

--- app/utils/chat/index.js
+++ app/utils/chat/index.js
@@ -12,6 +12,19 @@ module.exports = function(app) {
   		res.sendFile(__dirname + '/index.css');
 	})
 
+	app.get('/chat/jquery.js', function (req, res) {
+  		res.sendFile(__dirname + '/imports/jquery-2.2.2.min.js');
+	})
+
+	app.get('/chat/mCustomScrollbar.js', function (req, res) {
+  		res.sendFile(__dirname + '/imports/jquery.mCustomScrollbar.concat.min.js');
+	})
+
+	app.get('/chat/mCustomScrollbar.css', function (req, res) {
+  		res.sendFile(__dirname + '/imports/jquery.mCustomScrollbar.min.css');
+	})
+
+
 	app.get('/chat/chat.js', function (req, res) {
   		res.sendFile(__dirname + '/chat.js');
 	});
